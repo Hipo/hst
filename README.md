9e9e6332e27ffddd992fb79115a68416815586cb
ybrs <aybars.badur@gmail.com>
2015-06-19 22:00:53 +0300

    now we can install hst

---
+++ README.md
@@ -0,0 +1,2 @@
+
+[![asciicast](https://asciinema.org/a/14.png)](https://asciinema.org/a/7znbn9s0k3iorfnv6ekigww4h)
---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+VERSION = '0.1'
+DESCRIPTION = 'history picker in ncurses - also can pick other things'
+
+setup(
+    name='hst',
+    version=VERSION,
+    description=DESCRIPTION,
+    author='ybrs',
+    license='MIT',
+    url="http://github.com/ybrs/hst",
+    author_email='aybars.badur@gmail.com',
+    packages=['hst'],
+    install_requires=['pyperclip'],
+    scripts=['./hst/bin/hst'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
